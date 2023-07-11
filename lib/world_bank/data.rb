ea795de61078b1057c7401d2108fa4f1f1bf0aa9
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-07-11 17:16:08 -0500

    Wizardguy210/world_bank_ruby

--- lib/world_bank/data.rb
+++ lib/world_bank/data.rb
@@ -1,73 +1,73 @@
-module WorldBank
+# module WorldBank
 
-  class Data
+# class Data
 
-    attr_reader :raw, :name, :id, :value, :date, :others
+# attr_reader :raw, :name, :id, :value, :date, :others
 
-    def self.format(arg)
-      find('all').format(arg)
+# def self.format(arg)
+# find('all').format(arg)
     end
 
-    def self.id(arg)
-      find('all').id(arg)
+# def self.id(arg)
+# find('all').id(arg)
     end
 
-    def self.raw
-      find('all').raw
+# def self.raw
+# find('all').raw
     end
 
-    def self.most_recent_values(arg)
-      find('all').most_recent_values(arg)
+# def self.most_recent_values(arg)
+# find('all').most_recent_values(arg)
     end
 
-    def self.page(arg)
-      find('all').page(arg)
+# def self.page(arg)
+# find('all').page(arg)
     end
 
-    def self.per_page
-      find('all').per_page(arg)
+# def self.per_page
+# find('all').per_page(arg)
     end
 
-    def self.language(arg)
-      find('all').language(arg)
+# def self.language(arg)
+# find('all').language(arg)
     end
 
-    def self.income_level(arg)
-      find('all').income_level(arg)
+# def self.income_level(arg)
+# find('all').income_level(arg)
     end
 
-    def self.lending_type(arg)
-      find('all').lending_type(arg)
+# def self.lending_type(arg)
+# find('all').lending_type(arg)
     end
 
-    def self.region(arg)
+# def self.region(arg)
       find('all').region(arg)
     end 
 
-    def self.country(arg)
-      find('all').country(arg)
+# def self.country(arg)
+# find('all').country(arg)
     end
 
-    def self.fetch(arg)
-      find(arg).fetch
+# def self.fetch(arg)
+# find(arg).fetch
     end
 
-    def self.all
-      find('all')
+# def self.all
+# find('all')
     end
 
-    def self.find(id)
-      WorldBank::DataQuery.new('indicators', id, self)
+# def self.find(id)
+# WorldBank::DataQuery.new('indicators', id, self)
     end
 
-    def initialize(values={})
-      @raw = values
-      @name = values['indicator'].delete('value')
-      @id = values['indicator'].delete('id')
-      @value = values.delete('value')
-      @date = values.delete('date')
-      values.delete('indicator')
-      @others = values
+# def initialize(values={})
+# @raw = values
+# @name = values['indicator'].delete('value')
+# @id = values['indicator'].delete('id')
+# @value = values.delete('value')
+# @date = values.delete('date')
+# values.delete('indicator')
+# @others = values
     end
   end
 end
